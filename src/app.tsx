18e714c3b80435e316c5bf19feb37f1dbcc61fe8
_cjatoba <clayton.jatoba@watch.tv.br>
2024-02-10 17:51:08 -0300

    feat: add serach and save actions

--- src/app.tsx
+++ src/app.tsx
@@ -1,30 +1,70 @@
-import logo from './assets/logo-nlw-expert.svg';
-import {NoteCard} from "./components/note-card.tsx";
-import {NewNoteCard} from "./components/new-note-card.tsx";
+import logo from "./assets/logo-nlw-expert.svg";
+import { NoteCard } from "./components/note-card.tsx";
+import { NewNoteCard } from "./components/new-note-card.tsx";
+import { ChangeEvent, useState } from "react";
+
+interface Note {
+  id: string;
+  date: Date;
+  content: string;
+}
 
 export function App() {
+  const [search, setSearch] = useState("");
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnLocalStorage = localStorage.getItem("notes");
+
+    if (notesOnLocalStorage) {
+      return JSON.parse(notesOnLocalStorage);
+    }
+
+    return [];
+  });
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    };
+
+    const notesArray = [newNote, ...notes];
+
+    setNotes(notesArray);
+
+    localStorage.setItem("notes", JSON.stringify(notesArray));
+  }
+
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    setSearch(event.target.value);
+  }
+
+  const filteredNotes = notes.filter((note) =>
+    note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+  );
 
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
-      <img src={logo} alt="NLW Expert"/>
+      <img src={logo} alt="NLW Expert" />
 
       <form className="w-full">
         <input
           type="text"
           placeholder="Busque as suas notas..."
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+          onChange={handleSearch}
         />
       </form>
 
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <NewNoteCard />
-        <NoteCard note={{
-          date: new Date(),
-          content: "Aqui está o conteúdo da nota"
-        }} />
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+
+        {filteredNotes.map((note) => (
+          <NoteCard key={note.id} note={note} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
